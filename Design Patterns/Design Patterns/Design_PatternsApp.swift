9d3ab9e4037d8065f6bf51b2416471fa47159246
vit-cponce <carlosponce156@gmail.com>
2024-09-29 14:27:17 -0600

    Init project

---
+++ Design Patterns/Design Patterns/Design_PatternsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_PatternsApp.swift
+//  Design Patterns
+//
+//  Created by Carlos Ponce on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Design_PatternsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Design Patterns/Design PatternsTests/Design_PatternsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Design_PatternsTests.swift
+//  Design PatternsTests
+//
+//  Created by Carlos Ponce on 29/09/24.
+//
+
+import Testing
+@testable import Design_Patterns
+
+struct Design_PatternsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
